5a91ec1e23b36b793a2528640d603d24901a7244
John N. Norman <normanj777@gmail.com>
2022-05-25 07:38:57 -0700

    refactor search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,9 +3,9 @@ import './SearchBar.css'
 
 class SearchBar extends React.Component {
     render() {
-        return (<div class="SearchBar">
+        return (<div className="SearchBar">
         <input placeholder="Enter A Song, Album, or Artist" />
-        <button class="SearchButton">SEARCH</button>
+        <button className="SearchButton">SEARCH</button>
       </div>)
     }
 }
