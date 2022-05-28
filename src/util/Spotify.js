348642298984210443325370bb3716c22af69196
John N. Norman <normanj777@gmail.com>
2022-05-28 06:50:01 -0700

    add Spotify client method

---
+++ src/util/Spotify.js
@@ -0,0 +1,25 @@
+const clientId = "b4f885c091b64173b4be92af7f6e9471";
+const redirectUri = "http://fredjam.surge.sh/";
+const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
+let accessToken = undefined;
+let expiresIn = undefined;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+    const urlAccessToken = window.location.href.match(/access_token=([^&]*)/);
+    const urlExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
+    if (urlAccessToken && urlExpiresIn) {
+      accessToken = urlAccessToken[1];
+      expiresIn = urlExpiresIn[1];
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+    } else {
+      window.location = spotifyUrl;
+    }
+  },
+};
+
+export default Spotify;
